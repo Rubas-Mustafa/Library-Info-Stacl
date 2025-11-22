2bf031ceaa4c71ed8258ebc725c3804a9cb268b6
Syeda Nisa Batool <snisabatool1116@gmail.com>
2025-11-22 09:28:43 +0500

    Add files via upload

---
+++ book.h
@@ -0,0 +1,19 @@
+#ifndef BOOK_H
+#define BOOK_H
+
+struct Book {
+    int id;
+    char title[50];
+    char author[50];
+    int available; // 1 = available, 0 = borrowed
+};
+
+void addBook();
+void viewBooks();
+void searchBook();
+void updateBook();
+void deleteBook();
+int isBookAvailable(int bookID);
+
+#endif
+
